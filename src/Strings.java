699db4ad056040d52cde05a689d3609e92cde5df
Dmitry <pomelnikovda@gmail.com>
2022-07-28 21:41:13 +0300

    Initial commit

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int a = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"John");
+        System.out.println("My number is "+a);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3258;
+        long myLong = 2356454;
+
+        double myDouble = 235.55;
+        float myFloat = 2252.5f;
+
+        char a = 'a';
+        boolean b = true;
+
+        byte myByte = 100; //-128-127
+        System.out.println(myInt);
+    }
+}
